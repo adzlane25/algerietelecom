e9323a56bb5cc139ab2a853c7a5fba61acdce35b
Adlene <adzlane18@gmail.com>
2026-04-29 08:21:28 +0100

    add base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/algerietelecom/',   // ⬅️ أضفت هذا السطر (مهم جداً لـ GitHub Pages)
   server: {
     host: '0.0.0.0',
     allowedHosts: [
